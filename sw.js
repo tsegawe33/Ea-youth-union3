0d059f08794997c9615590583febe962028a24df
tsegawe33 <tsegacontentcreator@gmail.com>
2026-01-15 06:47:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
-const CACHE_NAME = 'ea-youth-ultimate-v1';
+const CACHE_NAME = 'ea-youth-ultimate-v9';
 const assets = [
   '/',
   '/index.html',
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+Ethiopic:wght@400;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', e => {

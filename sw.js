4df2a2cfd216d5ad2899c268a91bb3e79f605d38
tsegawe33 <tsegacontentcreator@gmail.com>
2026-01-15 05:44:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-const CACHE_NAME = 'ea-youth-pro-v2';
-const assets = [
-  './',
-  './index.html',
+const CACHE_NAME = 'ea-youth-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://fonts.googleapis.com/css2?family=Noto+Serif+Ethiopic:wght@400;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+Ethiopic:wght@400;700&display=swap'
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });

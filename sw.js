2e3771badaa83cf87d9d65a0b9c3adf522c27fbf
tsegawe33 <tsegacontentcreator@gmail.com>
2026-01-18 14:15:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,50 +1,10 @@
-const CACHE_NAME = 'ea-youth-union-v2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html', // የ HTML ፋይልህ ስም index.html ካልሆነ ቀይረው
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+Ethiopic:wght@400;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
-];
+const CACHE_NAME = 'ea-youth-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
-// መተግበሪያው ሲጫን ፋይሎችን በስልኩ ላይ ያከማቻል
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching essential assets...');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// አሮጌ ፋይሎችን ማጽጃ
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// ኢንተርኔት በሌለበት ጊዜ የተከማቹትን ፋይሎች ይጠቀማል
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((fetchResponse) => {
-        // አዳዲስ የሚመጡ ፋይሎችን (ለምሳሌ ምስሎች) ወደ ካሽ ይጨምራል
-        return caches.open(CACHE_NAME).then((cache) => {
-          if (event.request.url.startsWith('http')) {
-             cache.put(event.request, fetchResponse.clone());
-          }
-          return fetchResponse;
-        });
-      });
-    }).catch(() => {
-        // ኢንተርኔትም ካሽም ከሌለ (Offline)
-        console.log('Fetch failed; providing offline fallback.');
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
